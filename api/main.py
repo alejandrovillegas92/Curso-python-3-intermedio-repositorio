e5d8133a2b62ad67d76fac16230a8126e4ac5567
alejandrovillegas92 <avillegas973@gmail.com>
2023-08-03 22:24:56 -0600

    SE hciieron algunos cambios

---
+++ api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+
+def raiz():
+    return {'Hello':'World'}
+
+@app.get('/items/{item_id}/{m}')
+
+def read_items(item_id: int, m: str =None):
+    return {'item_id': item_id, 'm': m}
