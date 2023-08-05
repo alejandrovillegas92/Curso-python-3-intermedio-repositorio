7cd62809f9c479b231a4f91a0cb64fe8785ac230
alejandrovillegas92 <avillegas973@gmail.com>
2023-08-05 14:34:14 -0600

    Haciendo el trabajo final

--- api/Base de datos prueba.py
+++ api/Base de datos prueba.py
@@ -4,10 +4,24 @@ conn = sqlite3.connect("prueba.db")
 
 cursor = conn.cursor()
 
-cursor.execute ("""CREATE TABLE IF NOT EXIST Usuarios (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nombre TEXT NOT NULL,
-                    edad INTEGER) """)
+cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               nombre TEXT NOT NULL,
+               edad INTEGER
+)''')
 
 conn.commit()
 conn.close()
+
+conn = sqlite3.connect("prueba.db")
+cursor = conn.cursor()
+
+cursor.execute("INSERT INTO usuarios (nombre, edad) VALUES (?, ?)", ("Juan", 21))
+
+conn.commit()
+cursor.close()
+conn.close()
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
